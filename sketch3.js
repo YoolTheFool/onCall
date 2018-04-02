c3888c2e375630fbc44071a75590326f912c864e
Ayal Rosenberg <yool@172-16-240-244.DYNAPOOL.NYU.EDU>
2018-04-02 19:47:07 -0400

    first commit

---
+++ sketch3.js
@@ -0,0 +1,847 @@
+// function preload(){
+//   song1 = loadSound("fullNoLSM3.wav");
+//   song2 = loadSound("LSMNoChord.wav");
+//   song3 = loadSound("TUBE.wav");
+
+// }
+const time1 = 5;
+const time2 = 10;
+const time3 = 15;
+
+let order = [
+  {
+    text: 'A siren',
+    show_time: 1,
+    show_duration: 5,
+    height: 20,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You jump out of your bed',
+    show_time: 6,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Slide on your full-body hex-carbon suit',
+    show_time: 10,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You hear the muffled footsteps of your friends all around you',
+    show_time: 15,
+    show_duration: 4,
+    shown: false,
+    hidden: false
+  },
+  {
+    text: 'You grab your helmet and rush outside',
+    show_time: 19,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+
+  {
+    text: 'You wait for the elevator down to the hangar',
+    show_time: 24,
+    show_duration: 4,
+    shown: false,
+    hidden: false
+  },
+  {
+    text: 'You realize',
+    show_time: 28,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'This is no drill',
+    show_time: 30,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The greatest battle of your time will soon begin',
+    show_time: 33,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The way down to the hangar is long',
+    show_time: 44,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'In this claustrophobic trip, you try to calm your breath',
+    show_time: 47,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Loosen your muscles',
+    show_time: 52,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Your blood is thick liquid',
+    show_time: 54,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You have to be physically ready for the LSM shot',
+    show_time: 59,
+    show_duration: 6,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Before the elevator reaches its destination',
+    show_time: 65,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The floor vibrate, and you hear a distant hum',
+    show_time: 69,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'As the elevator doors open',
+    show_time: 73,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The hum turns to the soft whir of the MT-1s’ engines',
+    show_time: 75,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You put your helmet on, and activate the suit',
+    show_time: 80,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: "The suit's haptic-accelerometers are calibrating",
+    show_time: 84,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You feel them lightly push your skin',
+    show_time: 89,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You have FIVE SECONDS to decide if you are taking the LSM shot',
+    show_time: 93,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'TAKE THE SHOT?',
+    show_time: 98,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'To your right, you see the tubes for the LSM shot',
+    show_time: 103,
+    show_duration: 4,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'You plug the flexible tube to the back of your helmet',
+    show_time: 107,
+    show_duration: 5,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'The injection is sharp and painful',
+    show_time: 112,
+    show_duration: 3,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'You unplug yourself, and take a deep breath',
+    show_time: 115,
+    show_duration: 5,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'You run',
+    show_time: 122,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Alone',
+    show_time: 124,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'There is no co-pilot',
+    show_time: 127,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'There are no technicians',
+    show_time: 129,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Only pilots running to machines',
+    show_time: 132,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Running helps the LSM kick-in faster',
+    show_time: 135,
+    show_duration: 3,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'Around you, you hear engines switch on and whir',
+    show_time: 138,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You reach your MT',
+    show_time: 142,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You climb in',
+    show_time: 145,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Your silicon-sand seat immediately absorbs you',
+    show_time: 148,
+    show_duration: 6,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'After it finishes morphing, it hardens in a blink',
+    show_time: 154,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Power - on',
+    show_time: 158,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Engines - start',
+    show_time: 160,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Master weapon - SAFE',
+    show_time: 162.5,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Life support system - combat mode',
+    show_time: 165.5,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Closing canopy',
+    show_time: 170,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Quiet',
+    show_time: 175,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Warm sense is crawling up your forehead',
+    show_time: 179,
+    show_duration: 4,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'Flashing lights start to spin allover the huge room',
+    show_time: 183,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The hangar doors are opening',
+    show_time: 188,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You can see the air being sucked violently',
+    show_time: 191,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'As the blue glow of Neptune fills the hanger',
+    show_time: 196,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The first squadrons take off to the void',
+    show_time: 201,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You take the controls',
+    show_time: 206,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'As you touch them, you feel your nerve ends popping',
+    show_time: 211,
+    show_duration: 5,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'Your muscles are Titanium threads',
+    show_time: 216,
+    show_duration: 3,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'The LSM is on time',
+    show_time: 219,
+    show_duration: 3,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'You switch to hover mode and gravity drops',
+    show_time: 222,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You feel the subtle phase difference of your two engines',
+    show_time: 227,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'As your MT-1 leaves the hangar’s platform',
+    show_time: 231,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You see the screaming chasm of space under you',
+    show_time: 235,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You feel the LSM crawling to your temples',
+    show_time: 240,
+    show_duration: 4,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'You smoothly slide into warp formation',
+    show_time: 244,
+    show_duration: 4,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'This is it',
+    show_time: 248,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'You switch on the Hyper-Drive',
+    show_time: 250,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Holding',
+    show_time: 253,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Waiting for the command ',
+    show_time: 256,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'The LSM erupts',
+    show_time: 259,
+    show_duration: 3,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'Heart rate rising',
+    show_time: 262,
+    show_duration: 2,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'Pupils widening',
+    show_time: 264,
+    show_duration: 2,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'Matter dissolving',
+    show_time: 266,
+    show_duration: 2,
+    shown: false,
+    isLSM: true,
+    hidden: false
+  },
+  {
+    text: 'The cockpit is you',
+    show_time: 268,
+    show_duration: 2,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'Master weapons - ARM',
+    show_time: 270,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'I am ready to fight',
+    show_time: 273,
+    show_duration: 3,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  },
+  {
+    text: 'WARP',
+    show_time: 276,
+    show_duration: 5,
+    shown: false,
+    isLSM: false,
+    hidden: false
+  }
+]
+
+
+
+let started = false;
+let startTime = 0;
+let button1;
+let button2;
+let startButton;
+let buttons = [];
+let buttonsshowtime = 98;
+let buttonsduration = 5;
+let buttonsshown = false;
+let buttonshidden = false;
+let buttonsPressed = false;
+
+
+let title1;
+let title2;
+let isYes = false;
+
+//Visuals
+var bugs = []; // array of Jitter objects
+var length = 100
+
+
+
+  let clock = document.createElement('p');
+  clock.innerHTML = startTime;
+  clock.className = "visualText";
+  //document.body.appendChild(clock);
+
+function setup(){
+  //Visuals
+  // createCanvas(window.innerWidth, window.innerHeight);  // Create objects
+  // for (var i = 0; i < length; i++) {
+  //   bugs.push(new Jitter());
+  // }
+  //html
+  title1 = createP('"ON CALL"');
+  title1.class("visualText1");
+  title1.position(window.innerWidth/2 -90, window.innerHeight - 600);
+  title2 = createP("A pilot's story");
+  title2.class("visualText2");
+  title2.position(window.innerWidth/2 - 100, window.innerHeight - 500);
+  startButton = createButton('START');
+  startButton.position(window.innerWidth/2 + 10, window.innerHeight - 300);
+  startButton.mousePressed(letsGo);
+  button1 = createButton('YES');
+  button1.position(window.innerWidth/2 - 75, window.innerHeight - 200);
+  button1.mousePressed(buttonYES);
+  button1.hide();
+  button2 = createButton('NO');
+  button2.mousePressed(buttonNO);
+  button2.hide();
+  button2.position(window.innerWidth/2 + 65, window.innerHeight - 200);
+  buttons = [button1, button2];
+}
+
+function draw(){
+  //Visuals
+  background(0);
+
+
+  //html
+  let currentTime = millis()-startTime;
+  let currentSeconds = currentTime/1000;
+  clock.innerHTML = currentSeconds;
+
+
+
+  if(started){
+  //   if (currentSeconds > time1){
+  //   for (var i = 0; i < bugs.length; i++) {
+  //     bugs[i].move();
+  //     bugs[i].display1();
+  //   }
+  // }
+
+      if(currentSeconds > buttonsshowtime && !buttonsshown){
+        for(let i = 0; i < buttons.length;i++){
+          buttons[i].show();
+        }
+        buttonshown = true;
+      }
+      if((currentSeconds - buttonsshowtime > buttonsduration && !buttonshidden) || buttonsPressed){
+        for(let i = 0; i < buttons.length;i++){
+          buttons[i].hide();
+        }
+        buttonhidden = true;
+        if(!isYes){
+          tubePlayer.stop();
+          lsmPlayer.stop();
+          part.stop();
+        }
+      }
+
+
+
+
+    for(let i = 0; i < order.length;i++){
+      if (isYes){
+        order[i].isLSM = false;
+      }
+      if (!order[i].isLSM){
+        if(currentSeconds > order[i].show_time && !order[i].shown){
+          console.log("create P with text ", order[i].text);
+
+
+            let text = document.createElement('p');
+            text.id = "order+"+i;
+            text.className = "visualText";
+            //text.position = 200;
+            text.innerHTML = order[i].text;
+            document.body.appendChild(text);
+
+
+            order[i].shown = true;
+          }
+
+
+
+        if(currentSeconds - order[i].show_time > order[i].show_duration && !order[i].hidden){
+          console.log("hide ", order[i].text);
+
+          let element = document.getElementById("order+"+i);
+          element.parentElement.removeChild(element);
+
+          order[i].hidden = true;
+        }
+        if (isYes){
+          order[i].isLSM = false;
+        }
+    }
+  }
+}
+
+}
+var lsmPlayer = new Tone.Player("LSMChord.wav").toMaster();
+var tubePlayer = new Tone.Player("TUBE.wav").toMaster();
+var kickPlayer = new Tone.Player("newElectroKick2.wav").toMaster();
+
+var part = new Tone.Part(function(time, note) {
+kickPlayer.start(time);
+},[["0:0"],["0:1"],["0:2"],["0:3"]]);
+
+function letsGo(){
+  startButton.hide()
+  title1.hide();
+  title2.hide();
+
+var Player = new Tone.Player("fullNoLSM3.wav").toMaster();
+
+kickPlayer.volume.value = -12;
+
+
+part.loop = true;
+
+lsmPlayer.volume.value = 5;
+
+Tone.Buffer.on('load', function(){
+
+Tone.Transport.start(0)
+startVisuals();
+var now = Tone.now()
+      Player.start(now);
+      tubePlayer.start(now+105);
+      lsmPlayer.start(now+180);
+      part.start(now+260).stop(now+276)
+
+})
+}
+function startVisuals(){
+  // document.body.style.backgroundColor = "black";
+  started = true;
+  startTime = millis();
+
+  }
+
+
+
+function buttonNO(){
+  buttonsPressed = true;
+  buttonhidden = true;
+}
+
+function buttonYES(){
+  buttonsPressed = true;
+  buttonhidden = true;
+  isYes = true;
+}
+
+
+// Jitter class
+
+function Jitter() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(2, 50);
+  this.speed = 1;
+  //this.col = (255,0,0);
+  this.alpha = 2;
+  this.speedA = 1;
+  this.multi = [0.2,1.8];
+  this.alphaMax = 50;
+
+  this.move = function() {
+    this.x += random(-this.speed*random(this.multi), this.speed*random(this.multi));
+    this.y += random(this.speed*random(this.multi), -this.speed*random(this.multi));
+
+    if (this.x > width || this.x < 0) {
+      this.x = random(width);
+    }
+    if (this.y > height || this.y < 0) {
+      this.y = random(height);
+    }
+
+
+    };
+
+    this.display1 = function() {
+
+      noStroke();
+      fill(255,0,0,this.alpha);
+      ellipse(this.x, this.y, this.diameter, this.diameter);
+      this.alpha += this.speedA;
+
+      if (this.alpha > this.alphaMax || this.alpha < 0) {
+        this.speedA = - this.speedA;
+      }
+
+
+    };
+
+    this.display2 = function() {
+
+      noStroke();
+      fill(255,0,0,this.alpha);
+      ellipse(this.x, this.y, this.diameter, this.diameter);
+      this.alpha += this.speedA;
+
+      if (this.alpha > this.alphaMax || this.alpha < 0) {
+        this.speedA = - this.speedA;
+      }
+
+
+    };
+
+
+    // this.changeAlphaMax = function() {
+    //   this.alphaMax = 60;
+    //   //this.speedA += 0.01;
+    //
+    // };
+    // this.changeSpeedA = function() {
+    // this.speedA = 2;
+    //
+    // };
+
+  }
